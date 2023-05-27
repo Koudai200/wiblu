0dc22a5192d2a2c8727c8edc62647a465dc82a84
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-05-27 17:48:54 +0900

    Update application.js
    update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,36 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).ready(function() {
+    $('.like-button').click(function(e) {
+      e.preventDefault();  // デフォルトのクリックイベントをキャンセル
+  
+      var $button = $(this);
+      var resourceId = $button.data('resource-id');
+      var resourceType = $button.data('resource-type');
+  
+      $.ajax({
+        url: '/likes/toggle.js',  // サーバーサイドのエンドポイント（いいね！の追加と削除を切り替えるエンドポイント）
+        method: 'POST',
+        data: { resource_id: resourceId, resource_type: resourceType },  // リクエストのデータ（いいね！するリソースのIDとタイプ）
+        success: function(response) {
+          // レスポンスを処理してUIを更新する
+          if (response.liked) {
+            $button.removeClass('unliked').addClass('liked');
+            $button.find('.fa-heart').removeClass('unliked-btn').addClass('liked-btn');
+          } else {
+            $button.removeClass('liked').addClass('unliked');
+            $button.find('.fa-heart').removeClass('liked-btn').addClass('unliked-btn');
+          }
+          $button.closest('.post-like').find('.like-count').text(response.like_count);
+        },
+        error: function() {
+          alert('いいね！に失敗しました');
+        }
+      });
+    });
+  });
+  
+  
+ 
