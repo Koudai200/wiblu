c2eca81b28b73c88fee2413e5ec27cb51d478411
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-06-02 11:49:58 +0900

    Update unicorn.rb
    update

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,29 +1,55 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
+# rootパスのディレクトリを指定
+root_path = File.expand_path('../../', __FILE__)
+
+# アプリケーションサーバの性能を決定する
+worker_processes 2
+
+# アプリケーションの設置されているディレクトリを指定
+working_directory root_path
+
+# プロセスIDの保存先を指定
+pid "#{root_path}/tmp/pids/unicorn.pid"
+
+# ポート番号を指定
+listen "#{root_path}/tmp/sockets/unicorn.sock"
+
+# エラーのログを記録するファイルを指定
+stderr_path "#{root_path}/log/unicorn.stderr.log"
+
+# 通常のログを記録するファイルを指定
+stdout_path "#{root_path}/log/unicorn.stdout.log"
+
+#応答時間を待つ上限時間を設定
+timeout 30
+
+# ダウンタイムなしでUnicornを再起動時する
 preload_app true
 
-listen '/var/www/wiblu/tmp/unicorn.sock'
-pid '/var/www/wiblu/tmp/unicorn.pid'
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
+check_client_connection false
+
+run_once = true
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
+  defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
-end
 
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  if run_once
+    run_once = false # prevent from firing again
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
+    end
+  end
 end
 
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
