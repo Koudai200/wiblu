12296ce9a78e888f290d99ba3f38d0cbab04bdb4
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-06-06 12:16:54 +0900

    Update users_controller.rb
    add   def destroy
        @user = User.find_by(id: params[:id])
        @user.destroy
        flash[:notice] = "投稿を削除しました"
        redirect_to("/")
      end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -118,6 +118,13 @@ class UsersController < ApplicationController
     end
   end
   
+    def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/")
+  end
+  
     private
   
   def user_params
