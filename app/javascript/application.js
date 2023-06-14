c2dcadfe6246e87f5833aca9e6672dffa6515c9d
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-06-14 18:30:38 +0900

    Update application.js
    update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,36 +1,49 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-$(document).ready(function() {
-    $('.like-button').click(function(e) {
-      e.preventDefault();  // デフォルトのクリックイベントをキャンセル
-  
-      var $button = $(this);
-      var resourceId = $button.data('resource-id');
-      var resourceType = $button.data('resource-type');
-  
-      $.ajax({
-        url: '/likes/toggle.js',  // サーバーサイドのエンドポイント（いいね！の追加と削除を切り替えるエンドポイント）
-        method: 'POST',
-        data: { resource_id: resourceId, resource_type: resourceType },  // リクエストのデータ（いいね！するリソースのIDとタイプ）
-        success: function(response) {
-          // レスポンスを処理してUIを更新する
-          if (response.liked) {
-            $button.removeClass('unliked').addClass('liked');
-            $button.find('.fa-heart').removeClass('unliked-btn').addClass('liked-btn');
-          } else {
-            $button.removeClass('liked').addClass('unliked');
-            $button.find('.fa-heart').removeClass('liked-btn').addClass('unliked-btn');
-          }
-          $button.closest('.post-like').find('.like-count').text(response.like_count);
-        },
-        error: function() {
-          alert('いいね！に失敗しました');
-        }
-      });
+  $(document).ready(function() {
+    $(".user-tabs li").click(function() {
+      // クリックされたタブのIDを取得
+      var tabId = $(this).find("a").attr("href");
+      
+      // タブの切り替え
+      $(".tab-content").hide(); // すべてのコンテンツを非表示にする
+      $(tabId).show(); // クリックされたタブに対応するコンテンツを表示する
+      
+      // アクティブなタブのスタイルを設定
+      $(".user-tabs li").removeClass("active");
+      $(this).addClass("active");
+      
+      return false; // デフォルトのリンク遷移を無効化
     });
+
+     // 初期表示では「いいね！」タブのコンテンツを表示
+     $("#posts-tab").hide();
+    
+     // タブのクリックイベントハンドラ
+     $("#posts-tab-link").click(function() {
+       // クリックされたタブに対応するコンテンツを表示
+       $("#posts-tab").show();
+       $("#likes-tab").hide();
+       
+       // タブのスタイルを設定
+       $("#posts-tab-link").addClass("active");
+       $("#likes-tab-link").removeClass("active");
+       
+       return false;
+     });
+     
+     $("#likes-tab-link").click(function() {
+       // クリックされたタブに対応するコンテンツを表示
+       $("#posts-tab").hide();
+       $("#likes-tab").show();
+       
+       // タブのスタイルを設定
+       $("#posts-tab-link").removeClass("active");
+       $("#likes-tab-link").addClass("active");
+       
+       return false;
+     });
+
+     setTimeout(function() {
+      $('.flash').fadeOut('slow');
+    }, 2000);
+    
   });
-  
-  
- 
