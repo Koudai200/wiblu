f538184527e41ed06fc776f9c8cb50721bb70d18
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-07-14 14:08:39 +0900

    Update routes.rb
    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/search", to: "application#search"
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get "/search", to: "application#search"  
-
+ 
   get "signup", to: "users#new"
   post "users/create", to: "users#create"
   post "users/:id/update" => "users#update"
@@ -15,6 +13,7 @@ Rails.application.routes.draw do
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
   get "users/:id/destroy" => "users#destroy", as: "destroy_user"
+  get "users/:id/requests" => "users#requests"
 
   resources :users, except: [:show] do
     get :likes, on: :member
@@ -43,23 +42,43 @@ Rails.application.routes.draw do
   get "likes/:movie_id/movie_create" => "likes#movie_create", as: "movie_create_like"
   get "likes/:movie_id/movie_destroy" => "likes#movie_destroy", as: "movie_destroy_like"
 
-  post "images/:id/update" => "images#update", as: "update_image"
-  get "images/:id/destroy" => "images#destroy", as: "destroy_image"
-  resources :images do
-    resources :image_events, only: [:index]
-  end
-
   post "musics/:id/update" => "musics#update", as: "update_music"
   get "musics/:id/destroy" => "musics#destroy", as: "destroy_music"
   resources :musics do
     resources :music_events, only: [:index]
   end
 
+  post "images/:id/update" => "images#update", as: "update_image"
+  get "images/:id/destroy" => "images#destroy", as: "destroy_image"
+  resources :images do
+    resources :image_events, only: [:index]
+  end
+
   post "movies/:id/update" => "movies#update", as: "update_movie"
   get "movies/:id/destroy" => "movies#destroy", as: "destroy_movie"
+  
   resources :movies do
     resources :movie_events, only: [:index]
   end
+
+  resources :applies, only: [:index, :create, :destroy, :update]
+  get "applies/:id/index" => "applies#index"
+
+  post "images/:id/applies" => "applies#image_create"
+  post "images/:id/applies/update" => "applies#image_update"
+  delete "images/:id/applies" => "applies#image_destroy"
+
+  post "movies/:id/applies" => "applies#movie_create"
+  post "movies/:id/applies/update" => "applies#movie_update"
+  delete "movies/:id/applies" => "applies#movie_destroy"
+
+  post "musics/:id/applies" => "applies#music_create"
+  post "musics/:id/applies/update" => "applies#music_update"
+  delete "musics/:id/applies" => "applies#music_destroy"
+
+  post "posts/:id/applies" => "applies#post_create"
+  post "posts/:id/applies/update" => "applies#post_update"
+  delete "posts/:id/applies" => "applies#post_destroy"
 
   resources :messages, only: [:create]
   get "rooms/index" => "rooms#index"
