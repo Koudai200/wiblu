31b032d38917b4da7bb32232aa32662b04d40d9b
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-07-14 14:15:07 +0900

    Update music.rb
    add
        has_many :applies, dependent: :destroy
        has_many :users, through: :requests

--- app/models/music.rb
+++ app/models/music.rb
@@ -7,6 +7,9 @@ class Music < ApplicationRecord
 
     has_many :likes
 
+    has_many :applies, dependent: :destroy
+    has_many :users, through: :requests
+
     def self.search(keyword)
       where(["body like?", "%#{keyword}%"])
     end
