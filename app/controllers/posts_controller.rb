74379a56d386925323315fc03569ea6e80de6e1a
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-07-14 14:09:48 +0900

    Update posts_controller.rb
    add
          if @apply.nil?
            @apply = Apply.new
          else
            @apply = Apply.find_by(post_id: @post.id)
          end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,11 @@ class PostsController < ApplicationController
       @post = Post.find_by(id: params[:id])
       @user = User.find_by(id: @post.user_id)
       @likes_count = Like.where(post_id: @post).count
+      if @apply.nil?
+        @apply = Apply.new
+      else
+        @apply = Apply.find_by(post_id: @post.id)
+      end
     end
     
     def edit
