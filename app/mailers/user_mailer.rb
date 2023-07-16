655660c11ab1cafc39b627e82993ede566a283a9
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2023-07-16 15:55:58 +0900

    Update user_mailer.rb
    add
     host: "localhost:3000"

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   default from: "from@example.com"
   def reset_password_email(user)
     @user = User.find(user.id)
-    @url  = edit_password_reset_url(@user.reset_password_token)
+    @url  = edit_password_reset_url(@user.reset_password_token,  host: "https://wiblu.net")
     mail(to: user.email, subject: "パスワードリセット")
    end
 
