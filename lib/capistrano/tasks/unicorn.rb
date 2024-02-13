431b9aa281dad0acd5c3f386eadfae598b684cc6
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2024-02-13 10:39:50 +0900

    Update unicorn.rb
    update

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -1,67 +1,47 @@
-
-#unicornのpidファイル、設定ファイルのディレクトリを指定
 namespace :unicorn do
-    task :environment do
-      set :unicorn_pid,    "#{current_path}/tmp/pids/unicorn.pid"
-      set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
-    end
-  
-  #unicornをスタートさせるメソッド
-    def start_unicorn
-      within current_path do
-        execute :bundle, :exec, :unicorn, "-c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
-      end
-    end
-  
-  #unicornを停止させるメソッド
-    def stop_unicorn
-      execute :kill, "-s QUIT $(cat #{fetch(:unicorn_pid)})"
-    end
-  
-  #unicornを再起動するメソッド
-    def reload_unicorn
-      execute :kill, "-s USR2 $(cat #{fetch(:unicorn_pid)})"
-    end
-  
-  #unicronを強制終了するメソッド
-    def force_stop_unicorn
-      execute :kill, "$(< #{fetch(:unicorn_pid)})"
-    end
-  
-  #unicornをスタートさせるtask
-    desc "Start unicorn server"
-    task start: :environment do
-      on roles(:app) do
-        start_unicorn
-      end
-    end
-  
-  #unicornを停止させるtask
-    desc "Stop unicorn server gracefully"
-    task stop: :environment do
-      on roles(:app) do
-        stop_unicorn
-      end
-    end
-  
-  #既にunicornが起動している場合再起動を、まだの場合起動を行うtask
-    desc "Restart unicorn server gracefully"
-    task restart: :environment do
-      on roles(:app) do
-        if test("[ -f #{fetch(:unicorn_pid)} ]")
-          reload_unicorn
-        else
-          start_unicorn
-        end
-      end
-    end
-  
-  #unicornを強制終了させるtask
-    desc "Stop unicorn server immediately"
-    task force_stop: :environment do
-      on roles(:app) do
-        force_stop_unicorn
+  task :environment do
+    on roles(:app) do
+      within release_path do
+        fetch(:unicorn_pid)    { File.join(release_path, 'tmp', 'pids', 'unicorn.pid') }
+        fetch(:unicorn_config) { File.join(release_path, 'config', 'unicorn', 'production.rb') }
       end
     end
   end
-  
+
+  desc "Start unicorn server"
+  task start: :environment do
+    on roles(:app) do
+      within release_path do
+        execute :bundle, :exec, :unicorn, "-c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
+      end
+    end
+  end
+
+  desc "Stop unicorn server gracefully"
+  task stop: :environment do
+    on roles(:app) do
+      within release_path do
+        if test("[ -f #{fetch(:unicorn_pid)} ]")
+          execute :kill, "-s QUIT $(cat #{fetch(:unicorn_pid)})"
+        end
+      end
+    end
+  end
+
+  desc "Restart unicorn server gracefully"
+  task restart: :environment do
+    on roles(:app) do
+      invoke "unicorn:stop"
+      invoke "unicorn:start"
+    end
+  end
+
+  desc "Stop unicorn server immediately"
+  task force_stop: :environment do
+    on roles(:app) do
+      within release_path do
+        execute :kill, "$(cat #{fetch(:unicorn_pid)})"
+      end
+    end
+  end
+end
