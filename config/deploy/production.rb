a22c227133ad526e574cb98adcedc672887f3f13
Koudai200 <133979883+Koudai200@users.noreply.github.com>
2024-02-14 11:13:35 +0900

    Update production.rb
    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,35 +1,62 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.18.0"
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
 
-set :application, "Libeu_app"
-set :repo_url, "git@github.com:Koudai200/wiblu.git"
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
+server '160.251.210.130',
+  user: 'libeu',
+  roles: %w{app db web}, 
+  port: 22
+# role-based syntax
+# ==================
 
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
 
-# SSH接続設定
-set :ssh_options, {
-  auth_methods: ['publickey'], 
-  keys: ['~/.ssh/conoha_libeu/id_rsa'] 
-}
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
 
-# 保存しておく世代の設定
-set :keep_releases, 5
 
-# rbenvの設定
-set :rbenv_type, :user
-set :rbenv_ruby, '3.2.3'
 
-# ここからUnicornの設定
-# Unicornのプロセスの指定
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
 
-# Unicornの設定ファイルの指定
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
-# Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/user_name/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
